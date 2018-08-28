6dc48c1e4ab942738e00afd0f2bdece954f9b480
wangchao <wangchao@juewei.com>
2018-08-28 11:04:09 +0800

    this is first commit

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+require __DIR__ ."/vendor/autoload.php";
+
+use Hello\Example;
+
+$obj = new Example();
+
+$obj->run();
---
+++ src/example.php
@@ -0,0 +1,11 @@
+<?php
+namespace Hello;
+
+class Example{
+
+
+	public function run(){
+
+		echo "跑起来";
+	}
+}
